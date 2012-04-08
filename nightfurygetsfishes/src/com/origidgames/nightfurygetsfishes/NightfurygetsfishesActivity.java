963adc0bba033c9ca526654b5d0e860aa1f04e05
KhanhNV <KhanhNV@KhanhNV-RPC>
2012-04-09 00:03:01 +0900

    +)Created new project

---
+++ nightfurygetsfishes/src/com/origidgames/nightfurygetsfishes/NightfurygetsfishesActivity.java
@@ -0,0 +1,13 @@
+package com.origidgames.nightfurygetsfishes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NightfurygetsfishesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
