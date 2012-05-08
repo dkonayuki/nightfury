ecd754dbc1ed153c8bf64cf84ad5ffee5e3dac57
knightloving <hero_titan2006@yahoo.com>
2012-05-08 22:54:49 +0900

    layoutui2

--- nightfurygetsfishes/src/com/origidgames/nightfurygetsfishes/NightfurygetsfishesActivity.java
+++ nightfurygetsfishes/src/com/origidgames/nightfurygetsfishes/NightfurygetsfishesActivity.java
@@ -12,7 +12,7 @@ public class NightfurygetsfishesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         PublicResource.LoadResource(getBaseContext());
-        startActivity(new Intent("com.origidgames.nightfuryUI3"));
+        startActivity(new Intent("com.origidgames.nightfuryUI2"));
     }
     protected void onDestroy() {
     	super.onDestroy();
